2a07162082fea8715f71a3a099d37ddfcc506ad8
balinux <ryojuniyantara@gmail.com>
2021-02-05 21:13:29 +0800

    udpate: load data from trello api

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,12 +5,43 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
-  })
+// module.exports = function (api) {
+//   api.loadSource(({ addCollection }) => {
+//     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+//   })
 
-  api.createPages(({ createPage }) => {
-    // Use the Pages API here: https://gridsome.org/docs/pages-api/
+//   api.createPages(({ createPage }) => {
+//     // Use the Pages API here: https://gridsome.org/docs/pages-api/
+//   })
+// }
+
+const axios = require('axios')
+
+module.exports = function (api) {
+  api.loadSource(async actions => {
+    const { data } = await axios.get('https://trello.com/b/fgnRYaKZ/blog-trello.json')
+
+    // const collection = actions.addCollection('Post')
+
+    const posts = actions.addCollection({
+      typeName: 'TrelloPost'
+    })
+
+    for (const item of data.cards) {
+      posts.addNode({
+        id: item.id,
+        title: item.name,
+        content: item.desc,
+        image: item.attachments.length != 0 ? item.attachments[0].url : '',
+        labels: item.labels.map(label => label.name),
+        attachments: item.attachments.map(attachment => {
+          if (attachment.mimeType == '') {
+            return attachment.url
+          }
+        }),
+        date: item.dateLastActivity.split("T")[0]
+      })
+    }
   })
 }
+
