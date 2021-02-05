513077c13ee652fbedca7fae14f4a6cda0124289
balinux <ryojuniyantara@gmail.com>
2021-02-05 21:14:59 +0800

    add: tag trello

---
+++ src/components/WorkTagsTrello.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="work-tags">
+    <g-link class="work-tags-link" v-for="(label, index) in work.labels" :key="index">{{
+      label 
+    }}</g-link>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["work"],
+};
+</script>
+
+<style lang="scss">
+.work-tags {
+  margin: 1.5em 0 0;
+  position: absolute;
+  bottom: 40px;
+  z-index: 20;
+
+  &-link {
+    margin-right: 2em;
+    color: currentColor;
+    background-color: var(--card-tag-bg);
+    font-size: 0.7em;
+    border-radius: 25em;
+    padding: 0.15em 0.45em;
+  }
+}
+@media screen and (max-width: 625px) {
+  .work-tags {
+    font-size: 1.4rem;
+  }
+}
+</style>
